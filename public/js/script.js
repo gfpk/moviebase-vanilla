4cf3e712f63f8980dc9b83e4b71ef8490d63ef4a
Grzegorz Filipek <kongu3333@gmail.com>
2015-08-09 02:47:53 +0100

    the same as angular - no animation tho

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,6 @@ var load = {
 	    var xhr = new XMLHttpRequest();
 	    xhr.onreadystatechange = function()
 	    {
-	    	console.log("xhr.readyState === 4");
 	        if (xhr.readyState === 4 && xhr.status === 200) {
 	             if (success)
 	                    success(JSON.parse(xhr.responseText));
@@ -42,22 +41,84 @@ var load = {
 
 	successlog:function(data){
 		console.log(data );
-	}
+	},
+
+	populateData:function(htmlString, dataObj){
+		var toBeReplaced = ['id', 'title', 'description', 'genre', 'rating'];
+		var newhtmlString = htmlString;
+		for (var i = 0; i < toBeReplaced.length; i++) {
+			var key = toBeReplaced[i]
+			var expresion = '{{movie.' + toBeReplaced[i] + '}}';
+			newhtmlString = newhtmlString.replace(new RegExp(expresion, 'g'), dataObj[key]);		
+		};	
+		
+		return newhtmlString
+	},
 
 };
 
+storage = {};
 
 
+searchPhrase = '';
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-	
+var contentdiv = document.getElementById('contentarea');
+renderList = function()
+	{
+		load.loadHTML('partials/list.html', function(data){	
+			var htmlData = data;
+			contentdiv.innerHTML = data;
+			load.loadJSON('data/movies.json', function(data){
+				var jsonData = data;
+				storage.htmlData = htmlData;
+				storage.jsonData = jsonData;
+				storage.jsonData = (searchPhrase != "")? searchMovies(searchPhrase) : jsonData;
+				if(storage.jsonData[0]){
+					load.loadHTML('partials/listitem.html', function(data){
+						var htmlResult = ""; 
+						var htmlData = data;
+						for (var i = 0; i < storage.jsonData.length; i++) {
+							htmlResult += load.populateData(htmlData, storage.jsonData[i]);
+						};	
+					var listEl = document.getElementById('list');
+					listEl.innerHTML = htmlResult;
+					});
+				}else{
+					load.loadHTML('partials/noresults.html', function(data){
+						
+					var listEl = document.getElementById('list');
+					listEl.innerHTML = data;
+					});
+				}			
+			});			
+		});
+	}
+renderList();	
 
-	load.loadHTML('partials/list.html', function(data){
-		
-		document.getElementById('contentarea').innerHTML = data;
-	});
+renderDetailed = function(item){
+	load.loadHTML('partials/detailed.html', function(data){	
+			var htmlData = data;
+			load.loadJSON('data/movies.json', function(data){
+				var jsonData = data;
+				storage.htmlSingleView = htmlData;
+				storage.jsonData = jsonData;			
+				var	htmlResult = load.populateData(htmlData, storage.jsonData[item]);			
+				contentdiv.innerHTML = htmlResult;		
+			});			
+		});
+}
 
-	var x = function(data){};
+searchMovies = function(phrase){
+	var results = [];
+	for (var i = 0; i < storage.jsonData.length; i++) {
+		if(storage.jsonData[i].title.toLowerCase().indexOf(phrase.toLowerCase()) > -1 ){
+			results.push(storage.jsonData[i]);
+		}
+	};
 	
+	return results
+}
+
 });
 

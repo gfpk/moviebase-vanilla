db2b7fa80cf4dfbe442af0ad9d559c6e6f4953bc
Grzegorz Filipek <kongu3333@gmail.com>
2015-08-10 00:17:38 +0100

    Vanilla version done, to not touch it anymore

--- public/js/script.js
+++ public/js/script.js
@@ -43,15 +43,18 @@ var load = {
 		console.log(data );
 	},
 
-	populateData:function(htmlString, dataObj){
-		var toBeReplaced = ['id', 'title', 'description', 'genre', 'rating'];
+	populateData:function(htmlString, dataObj, context){
+		var toBeReplaced = ['id', 'title', 'description', 'genre', 'rating', 'budget', 'audience', 'profits', 'trivia'];
 		var newhtmlString = htmlString;
 		for (var i = 0; i < toBeReplaced.length; i++) {
-			var key = toBeReplaced[i]
+			var key = toBeReplaced[i];
 			var expresion = '{{movie.' + toBeReplaced[i] + '}}';
-			newhtmlString = newhtmlString.replace(new RegExp(expresion, 'g'), dataObj[key]);		
-		};	
-		
+			if(key == "description" && context == "list"){
+				newhtmlString = newhtmlString.replace(new RegExp(expresion, 'g'), (dataObj[key].substring(0, 100) + "..."));
+			}else{
+				newhtmlString = newhtmlString.replace(new RegExp(expresion, 'g'), dataObj[key]);
+			}				
+		};			
 		return newhtmlString
 	},
 
@@ -59,8 +62,46 @@ var load = {
 
 storage = {};
 
+renderRating = function(){
+	var ratingElements = document.getElementsByClassName('rating');
+	for (var i = 0; i < ratingElements.length; i++) {
+		var r = parseInt(ratingElements[i].getAttribute('rating'));
+		var ratingHtml = "rating: "
+		for (var j = 1; j <= 5; j++) {
+			if(j <= r){
+				ratingHtml += "<i class='glyphicon glyphicon-star'></i>";
+			}else{
+				ratingHtml += "<i class='glyphicon glyphicon-star-empty'></i>";
+			};
+		};
+		ratingElements[i].innerHTML = ratingHtml;		
+	};
+};
+
+hideGallery = function(){
+	var e = document.getElementById("canvas");
+	var eInner = document.getElementById("canvasinner");
+	eInner.innerHTML ="";
+	e.className = ""; 
+};
+revealGallery = function(index){
+	galleryLightBox(index);
+	var e = document.getElementById("canvas");
+	e.className = "active"; 
+};
+
+galleryLightBox =function(index){
+	var canvas = document.getElementById("canvas");
+
+	var galleryItem = document.getElementsByClassName("galleryItem")[index];
+	var e = document.getElementById("canvasinner");
+	var galleryHtml = "<img src='" + galleryItem.src +"'>";
+	console.log(galleryItem);
+	e.innerHTML = galleryHtml;
+}
 
 searchPhrase = '';
+genreFilter = '';
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 var contentdiv = document.getElementById('contentarea');
@@ -69,20 +110,30 @@ renderList = function()
 		load.loadHTML('partials/list.html', function(data){	
 			var htmlData = data;
 			contentdiv.innerHTML = data;
+			if(genreFilter != ""){
+				
+				var filtersHtml = "<p>filter: <span class='badge badge-important'>"+ genreFilter +" <a href='/'><i class='glyphicon glyphicon-remove-circle'></i></a></span></p>";
+				var filterWrap = document.getElementById('filterwrap');
+				filterWrap.innerHTML =filtersHtml;
+
+			}
 			load.loadJSON('data/movies.json', function(data){
 				var jsonData = data;
 				storage.htmlData = htmlData;
 				storage.jsonData = jsonData;
 				storage.jsonData = (searchPhrase != "")? searchMovies(searchPhrase) : jsonData;
+				storage.jsonData = (genreFilter != "")? searchMoviesByGenre(genreFilter) : storage.jsonData;
 				if(storage.jsonData[0]){
 					load.loadHTML('partials/listitem.html', function(data){
 						var htmlResult = ""; 
 						var htmlData = data;
 						for (var i = 0; i < storage.jsonData.length; i++) {
-							htmlResult += load.populateData(htmlData, storage.jsonData[i]);
+							htmlResult += load.populateData(htmlData, storage.jsonData[i], "list");
 						};	
 					var listEl = document.getElementById('list');
+					
 					listEl.innerHTML = htmlResult;
+					renderRating();
 					});
 				}else{
 					load.loadHTML('partials/noresults.html', function(data){
@@ -103,9 +154,11 @@ renderDetailed = function(item){
 				var jsonData = data;
 				storage.htmlSingleView = htmlData;
 				storage.jsonData = jsonData;			
-				var	htmlResult = load.populateData(htmlData, storage.jsonData[item]);			
-				contentdiv.innerHTML = htmlResult;		
-			});			
+				var	htmlResult = load.populateData(htmlData, storage.jsonData[item], "detail");			
+				contentdiv.innerHTML = htmlResult;
+				renderRating();
+				galleryLightBox();			
+			});		
 		});
 }
 
@@ -118,7 +171,17 @@ searchMovies = function(phrase){
 	};
 	
 	return results
-}
+};
+searchMoviesByGenre = function(genre){
+	var results = [];
+	for (var i = 0; i < storage.jsonData.length; i++) {
+		if(storage.jsonData[i].genre === genre){
+			results.push(storage.jsonData[i]);
+		}
+	};
+	return results
+};
+
 
 });
 
